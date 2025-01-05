ae7cd8e60e83433d2bc0a2994c90f9e611cd3ca2
jennydai2011@gmail.com <jennydai2011@gmail.com>
2025-01-05 14:44:42 -0700

    demo

---
+++ Springboot3/src/test/java/com/jdai/Springboot3/Springboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdai.Springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
