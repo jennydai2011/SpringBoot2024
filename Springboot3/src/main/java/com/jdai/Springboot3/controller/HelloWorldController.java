10bb7486a967dd092580fc3198a9878acf537f7c
jennydai2011@gmail.com <jennydai2011@gmail.com>
2025-03-16 08:57:38 -0600

    www

--- Springboot3/src/main/java/com/jdai/Springboot3/controller/HelloWorldController.java
+++ Springboot3/src/main/java/com/jdai/Springboot3/controller/HelloWorldController.java
@@ -1,4 +1,13 @@
 package com.jdai.Springboot3.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello world 2025!";
+    }
 }
